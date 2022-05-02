817b00f5a6d76cd883214fc9eb07168569235c73
Zagoman <lucaszago.ph@gmail.com>
2022-05-02 11:49:17 +0200

    cleaning up product-list.js

--- product-list.js
+++ product-list.js
@@ -1,37 +1,43 @@
 window.addEventListener("DOMContentLoaded", dataFetch);
 
-function init(data) {
-  const template = document.querySelector("#product-temp").content;
-  const grid = document.querySelector(".product-list");
-  data.forEach((el) => showProduct(el, grid, template));
-  console.log(template);
+class ProductList {
+  constructor(data) {
+    this._template = document.querySelector("#product-temp").content;
+    this._grid = document.querySelector(".product-list");
+    this._Init(data);
+  }
+
+  _Init(data) {
+    data.forEach((el) => this._ShowProduct(el, this._grid, this._template));
+  }
+
+  _ShowProduct(el, parent, template) {
+    const clone = template.cloneNode(true);
+    //   adding the image
+    clone.querySelector("img").src = el.thumbnail.guid;
+    clone.querySelector("img").alt = el.productname;
+
+    //   adding the Titles
+    clone.querySelector("h2").textContent = el.productname;
+    clone.querySelector(".price-tag > span").textContent = el.price;
+
+    // Adding color
+    clone.querySelector(".product-color div").style.backgroundColor = el.colors;
+
+    // Feed the link
+    clone.querySelector("a").href = `./product.html?product=${el.id}`;
+    clone.querySelector("a.--link").href = `./product.html?product=${el.id}`;
+
+    parent.appendChild(clone);
+  }
 }
 
-function showProduct(el, parent, template) {
-  const clone = template.cloneNode(true);
-  //   adding the image
-  clone.querySelector("img").src = el.thumbnail.guid;
-  clone.querySelector("img").alt = el.productname;
-
-  //   adding the Titles
-  clone.querySelector("h2").textContent = el.productname;
-  clone.querySelector(".price-tag > span").textContent = el.price;
-
-  // Adding color
-  clone.querySelector(".product-color div").style.backgroundColor = el.colors;
-
-  // Feed the link
-  clone.querySelector("a").href = `./product.html?product=${el.id}`;
-  clone.querySelector("a.--link").href = `./product.html?product=${el.id}`;
-
-  parent.appendChild(clone);
-}
-
+let _productList = null;
 async function dataFetch() {
   const res = await fetch(
     "https://lucaszago.dk/silfen-wp/wp-json/wp/v2/product?_embed"
   );
   const data = await res.json();
 
-  init(data);
+  _productList = new ProductList(data);
 }
